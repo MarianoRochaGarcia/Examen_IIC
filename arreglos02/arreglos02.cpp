97703ffa4c8fd7fe92c2a4630fb7e1e62d0afa07
Rocha03 <marianorocha03@gmail.com>
2022-11-15 20:53:13 -0600

    CYP

---
+++ arreglos02/arreglos02.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main() {
+
+	/*float calificaciones[5];
+	float promedio = 0;
+	
+	printf("Programa que imprime el promedio de 5 calificaciones\n\n");
+
+	for (int i = 0; i < 5; i++) {
+		printf("Ingresa el valor de la calificacion %i: ", i + 1);
+		scanf_s("%f", &calificaciones[i]);
+
+		promedio += calificaciones[i]/5;
+
+	}
+	printf("El promedio de las 5 calificaciones es: %.2f\n\n", promedio);
+	return 0;*/
+	
+
+
+	/* 2 errores :) 14/11/22
+	float calificaciones[5];
+	float promedio = 0;
+	
+	printf("Programa que calcula el promedio de 5 calificaciones\n\n");
+
+	for (int i = 0; i < 5; i++) {
+		printf("Ingresa la califacion %i : ", i+1);
+		
+		scanf_s("%f", &calificaciones[i]);
+		
+		printf("\n");
+
+		promedio += calificaciones[i]/5;
+
+	}
+	printf("El promedio de la calificacion es : %.2f", promedio);*/
+}
